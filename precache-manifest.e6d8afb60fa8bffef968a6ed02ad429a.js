45b07b7745ac6d4b8c756ef63b94f5a9b0671e49
SE7EN <pudj.nesterov2004@mail.ru>
2023-05-12 21:36:29 +0800

    Изменение SPA

---
+++ precache-manifest.e6d8afb60fa8bffef968a6ed02ad429a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b17c2640aa7b5403b2c3",
+    "url": "./static/css/main.f3f8abb7.chunk.css"
+  },
+  {
+    "revision": "b17c2640aa7b5403b2c3",
+    "url": "./static/js/main.d65cac19.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8996a3587033242a6520",
+    "url": "./static/js/2.0d7dd752.chunk.js"
+  },
+  {
+    "revision": "d667b5dcd07f0503f5819aa64d875bbc",
+    "url": "./index.html"
+  }
+];
